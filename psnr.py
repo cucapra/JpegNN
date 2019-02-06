a5763705558a77fa9662c32de1234e352358adf6
Zhijing Li <zl679@gorgonzola.cs.cornell.edu>
2019-02-06 17:13:44 -0500

    psnr

--- psnr.py
+++ psnr.py
@@ -1,8 +1,6 @@
 from PIL import Image, ImageFilter
 import numpy as np
 import math
-
-
 def psnr(compressed,origin):
 
     mse = 0
@@ -18,15 +16,22 @@ def psnr(compressed,origin):
     res = 10 * math.log10(math.pow(255,2)/mse)
 
     return res
+'''
+def compressJ(im, name):
+    im.save(name, 'JPEG')
+    im = Image.open(name)
+    return im
 
-
-#Read image
-im = Image.open( 'hymenoptera_data/train/ants/0013035.jpg' )
-#Display image
-#im.show()
-#Saving the filtered image to a new file
-im.save( 'compress2jpg.jpg', 'JPEG' )
-cmp_im = Image.open('compress2jpg.jpg')
-im = np.array(im,np.int16).transpose(2,0,1)
-cmp_im = np.array(cmp_im,np.int16).transpose(2,0,1)
-print("PSNR: ", psnr(im[0], cmp_im[0]))
+##################################
+#example
+##################################
+##Read image
+#im = Image.open( 'hymenoptera_data/train/ants/0013035.jpg' )
+##Display image
+##im.show()
+##Saving the filtered image to a new file
+#cmp_im = compressJ(im, 'compress2jpg.jpg')
+#im = np.array(im,np.int16).transpose(2,0,1)
+#cmp_im = np.array(cmp_im,np.int16).transpose(2,0,1)
+#print("PSNR: ", psnr(im[0], cmp_im[0]))
+'''
