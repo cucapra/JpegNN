add loss function and fix gradient

cnn.py
@@ -231,6 +231,16 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
                     # Special case for inception because in training it has an auxiliary output. In train
                     #   mode we calculate the loss by summing the final output and the auxiliary output
                     #   but in testing we only consider the final output.
+                    
+                    #add regularization
+                    l1_crit = nn.L1Loss(size_average=False)
+                    reg_loss = 0
+                    factor = 0.005
+                    target = torch.zeros(3,8,8).cuda()
+                    for name, param in model.named_parameters():
+                        if name == "0.quantize":
+                            reg_loss += l1_crit(param,target)
+
                     if is_inception and phase == 'train':
                         # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
                         outputs, aux_outputs = model(inputs)
@@ -241,15 +251,17 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
                         outputs = model(inputs)
                         loss = criterion(outputs, labels)
 
+                    loss += (factor / reg_loss)
+
                     _, preds = torch.max(outputs, 1)
 
                     # backward + optimize only if in training phase
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
-                    #for name, param in model.named_parameters():
-                        #if name=="0.quantize":
-                        #    print(name, param.data)
+                        for name, param in model.named_parameters():
+                            if name == "0.quantize":
+                                print(param.grad)
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
@@ -661,6 +673,8 @@ model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft
 
 
 #print the trained quantization matrix
+name, param = model_ft[0]
+print(name, param)
 if args.qtable:
     print('--------- the trained quantize table ---------')
     for name,param in model_ft.named_parameters():

jpeg_layer.py
@@ -5,6 +5,7 @@ import numpy as np
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 import torch_dct as dct
+import gradient
 
 class JpegLayer(torch.nn.Module):
     def __init__(self, rand_qtable = True, block_size = 8, quality = 75):
@@ -28,7 +29,7 @@ class JpegLayer(torch.nn.Module):
         [99,99,99,99,99,99,99,99],
         [99,99,99,99,99,99,99,99]     
         ])/255
-        if rand_qtable:
+        if rand_qtable == False:
             quantize = np.array([quantizeY, quantizeC, quantizeC])
             self.quantize = torch.nn.Parameter(torch.cuda.FloatTensor(quantize))
         else:
@@ -39,6 +40,9 @@ class JpegLayer(torch.nn.Module):
         self.quality = self.__scale_quality(quality)
         self.bs = block_size
         self.dctmtx = self.__dctmtx(self.bs)
+        #gradients
+        self.round = gradient.RoundNoGradient
+        self.clamp = gradient.ClampNoGradient 
     def forward(self, input):
         
         #preprocessing
@@ -56,37 +60,29 @@ class JpegLayer(torch.nn.Module):
             dcts = torch.matmul(torch.matmul(self.dctmtx,sts2),self.dctmtx.t())
             #dcts = dct.dct_2d(sts2)
         #quantization
-            comp = torch.round(dcts/
-            torch.round( torch.clamp(
-            torch.round( (
-            torch.round(self.quantize[0]*255)
+            comp = self.round.apply(dcts/
+            self.round.apply( self.clamp.apply(
+            self.round.apply( (
+            self.round.apply(self.quantize[0]*255)
             *self.quality+50)/100)
-            ,min=1,max=255) ) 
+            ) ) 
             )
-            #torch.round(dcts/
-            #torch.round(#torch.clamp( 
-            #torch.round( (
-            #torch.round(self.quantize*255)
-            #*self.quality+50 ) /100)
-            #,min=1, max=255)
-            #) )
         
         #decompression
             nograd_quantize = \
-            torch.round(torch.clamp(
-            torch.round( (
-            torch.round(self.quantize[0].clone()*255)
+            self.round.apply(self.clamp.apply(
+            self.round.apply( (
+            self.round.apply(self.quantize[0].clone()*255)
             *self.quality+50 ) /100)
-            ,min=1,max=255
             ) )
-            decomp =  torch.round(comp*nograd_quantize)
+            decomp =  self.round.apply(comp*nograd_quantize)
             idcts = torch.matmul(torch.matmul(self.dctmtx.t(), decomp), self.dctmtx)
             #idcts = dct.idct_2d(decomp)
             sts3 = torch.cat(torch.unbind(idcts, 2), 3)
             sts4 = torch.cat(torch.unbind(sts3, 1), 1)
             samples2.append(sts4)
         ycbcr2 = self.__upsample(samples2)
-        y_pred = torch.round(self.__ycbcr2rgb(ycbcr2+128))
+        y_pred = self.round.apply(self.__ycbcr2rgb(ycbcr2+128))
 #        a,b,c,d = y_pred.shape
 #        for i in range(a):
 #            for j in range(b):
@@ -122,6 +118,7 @@ class JpegLayer(torch.nn.Module):
                     dct_mtx[i][j] = math.sqrt(2/bs) * \
                     math.cos((math.pi*(2*j+1)*i)/(2*bs)) 
         return dct_mtx.type(torch.cuda.FloatTensor)
+
     def __upsample(self, samples, row=2, col=2):
         upsamples = []
         upsamples.append(samples[0])
@@ -135,6 +132,7 @@ class JpegLayer(torch.nn.Module):
             upsamples.append(sts2)
         sts3 = torch.stack(upsamples)
         return sts3.permute(1,0,2,3)
+
     def __subsample(self, im, row=2, col=2):
         permute = im.permute(1,0,2,3)
         
@@ -158,7 +156,7 @@ class JpegLayer(torch.nn.Module):
         ycbcr = torch.matmul(ycbcr, xform.t())
         ycbcr[:,:,:,[1,2]] += 128
         #put mask
-        ycbcr = torch.clamp(ycbcr, min=0, max=255)
+        ycbcr = self.clamp.apply(ycbcr)
         ycbcr = ycbcr.permute(0,3,1,2)
         return ycbcr
 
@@ -171,7 +169,7 @@ class JpegLayer(torch.nn.Module):
         rgb[:,:,:,[1,2]] -= 128
         rgb = torch.matmul(rgb, xform.t())
         #put mask
-        rgb = torch.clamp(rgb, min=0, max=255)
+        rgb = self.clamp.apply(rgb)
         
         rgb = rgb.permute(0,3,1,2)
         return rgb