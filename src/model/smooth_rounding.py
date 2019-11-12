160a3d32935715b1c93d24fd0c14d05a62f39c4b
Zhiyong Hao <zh272@gorgonzola.cs.cornell.edu>
2019-11-11 20:31:53 -0500

    enable divisor gradient

--- src/model/smooth_rounding.py
+++ src/model/smooth_rounding.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 # def Rd(x,q=5):
 #     return round(x/q)
 
-def smoothRd(_input,q=5, scale=100.):
+def smoothRd(_input,q=5., scale=100.):
+    # smaller scale yields smoother outputs
+
     # For python or numpy:
     # base = _input//q
     # rem = _input-base*q
@@ -22,15 +24,22 @@ def smoothRd(_input,q=5, scale=100.):
     # smooth_rd = 1 / (1 + math.exp(-delta*scale))+offset # sigmoid
 
     # For pytorch
-    rem = _input%q
+    # https://github.com/pytorch/pytorch/blob/master/torch/csrc/jit/symbolic_script.cpp
+    # rem = _input%q
+    rem = torch.fmod(_input,q) # sign same as _input
+    rem[rem<0] += q[rem<0]
+    # rem = torch.remainder(_input,q) # sign same as q
     base = (_input-rem)/q
 
     delta = rem.clone()
-    offset = torch.ones(size=rem.shape, dtype=rem.dtype)*0.5
+    if isinstance(q, torch.Tensor):
+        delta[rem>q/2] = rem[rem>q/2]-q[rem>q/2]
+    else:
+        delta[rem>q/2] = rem[rem>q/2]-q
 
-    delta[rem>q/2] = rem[rem>q/2]-q
-    offset[rem<=q/2] *= -1
-    smooth_rd = 1 / (1 + torch.exp(-delta*scale/q))+offset # sigmoid
+    offset = rem.clone().fill_(0.5)
+    offset[rem<=q/2] = -0.5
+    smooth_rd = torch.sigmoid(delta*scale/q) + offset
 
     return base+smooth_rd
 
@@ -44,9 +53,14 @@ if __name__ == '__main__':
     # plt.savefig('test2.png')
     # plt.close()
 
-    x = torch.arange(1000)/20.-25
-    y = smoothRd(x)
-    plt.scatter(x, y)
+    # x = torch.arange(1000)/20.-25
+    x = torch.arange(40)/4. - 10.
+    # x.requires_grad = True
+    q = x.clone().fill_(5.)
+    q.requires_grad = True
+    y = smoothRd(x, q=q, scale=100.)
+    assert q.requires_grad==y.requires_grad
+    plt.scatter(x.detach().cpu(), y.detach().cpu())
     plt.savefig('test.png')
     plt.close()
 
